96b22afe5b56a39325d4090a5491c5892d6688b5
Andrei Martyanov <andrei.martianoff92@yandex.ru>
2026-08-23 01:40:04 +0300

    First step to refactoring

---
+++ fit-bridge-be/training-service/training-plan/tp-common/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("build-kmp")
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.kotlinx.datetime)
+                api("com.github.martyanovav.otuskotlin.fitbridge.libs:fit-bridge-lib-logging-common")
+                implementation(libs.coroutines.core)
+                implementation(libs.kotlinx.serialization.json)
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        jvmTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
